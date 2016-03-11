c33dec52f0cfb6e34c9242a52d787ac601960c3b
abie115 <donicczka@gmail.com>
2016-03-11 11:14:05 +0100

    dodanie formularza logowania

--- src/main/java/com/vaadin/abiewska/view/LoginView.java
+++ src/main/java/com/vaadin/abiewska/view/LoginView.java
@@ -2,7 +2,12 @@ package com.vaadin.abiewska.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class LoginView extends VerticalLayout implements View {
@@ -14,10 +19,31 @@ public class LoginView extends VerticalLayout implements View {
 
 	public void setUp() {
 
-		final TextField name = new TextField();
-		name.setCaption("Logowanie test");
+		Button btnLogin = new Button("Zaloguj");
+		Button btnRegister = new Button("Zarejestruj się");
+		TextField txtLogin = new TextField("Login");
+		PasswordField pfPassword = new PasswordField("Hasło");
 
-		this.addComponent(name);
+		Panel panel = new Panel("Logowanie");
+		panel.setSizeUndefined();// dopasowuje do zawartosci
+		this.addComponent(panel);
+
+		FormLayout formLogin = new FormLayout();
+		formLogin.addStyleName("login-form");
+		formLogin.addComponent(txtLogin);
+		formLogin.addComponent(pfPassword);
+		formLogin.addComponent(btnLogin);
+		formLogin.addComponent(btnRegister);
+		formLogin.setMargin(true);
+		formLogin.setSpacing(true);
+		panel.setContent(formLogin);
+
+		btnLogin.addClickListener(e -> {
+			String login = txtLogin.getValue();
+			String password = pfPassword.getValue();
+			UI.getCurrent().getNavigator().navigateTo("main");
+		});
+
 		setMargin(true);
 		setSpacing(true);
 
